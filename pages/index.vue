6fdc8dbeece96a63f525325bcaa0ad6301de3eec
FallenPhoenix8 <tplayxyz8@gmail.com>
2025-02-16 16:38:05 +0100

    Add Heading sizing 1-3

--- pages/index.vue
+++ pages/index.vue
@@ -10,5 +10,9 @@ const route = useRoute()
       >Learn more about Nuxt Routing</a
     >
     <TestMotion />
+
+    <h1>Heading 1</h1>
+    <h2>Heading 2</h2>
+    <h3>Heading 3</h3>
   </div>
 </template>
