84b299d1f35932d01d41b97ee9ad1cb5db51097f
FallenPhoenix8 <tplayxyz8@gmail.com>
2025-02-16 15:57:33 +0100

    Add motionjs

---
+++ components/TestMotion.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { animate } from "motion"
+
+async function onEnter(el: Element, onComplete: () => void) {
+  await animate(el, { opacity: 1 })
+  onComplete()
+}
+
+const count = ref(0)
+</script>
+
+<template>
+  <Transition :css="false" @enter="onEnter">
+    <h1 v-if="count > 0">Hello world</h1>
+  </Transition>
+  <button @click="() => count++" class="block">Click me!</button>
+</template>
+
+<style scoped>
+h1 {
+  opacity: 0;
+}
+</style>
