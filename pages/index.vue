9cbb06175b63ab140298efe80fca55ce3d58f622
FallenPhoenix8 <tplayxyz8@gmail.com>
2025-02-16 16:59:45 +0100

    Add link styling and change font size

--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,12 @@ const route = useRoute()
 <template>
   <div>
     <h1 class="text-red-500">Nuxt Routing set up successfully!</h1>
+    <p>
+      API_URL:
+      <a :href="useRuntimeConfig().public.apiUrl">{{
+        useRuntimeConfig().public.apiUrl
+      }}</a>
+    </p>
     <p>Current route: {{ route.path }}</p>
     <a href="https://nuxt.com/docs/getting-started/routing" target="_blank"
       >Learn more about Nuxt Routing</a
