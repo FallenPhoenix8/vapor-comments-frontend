87705dceab408ffd7254ffd3406ab0fb279df65a
FallenPhoenix8 <tplayxyz8@gmail.com>
2025-02-17 19:03:54 +0100

    Handle fetch failed errors in `get-user.ts`

--- server/api/get-user.ts
+++ server/api/get-user.ts
@@ -1,17 +1,21 @@
 import User from "~/class/User"
 
 export default defineEventHandler(async (event) => {
-  const apiUrl = useRuntimeConfig().public.apiUrl
-  const response = await fetch(`${apiUrl}/auth/me`, {
-    headers: event._headers,
-  })
+  try {
+    const apiUrl = useRuntimeConfig().public.apiUrl
+    const response = await fetch(`${apiUrl}/auth/me`, {
+      headers: event._headers,
+    })
 
-  if (response.status === 401) {
+    if (response.status === 401) {
+      return new User(null, null, false)
+    } else if (response.ok !== true) {
+      throw new Error("Failed to fetch user")
+    }
+
+    const user = await response.json()
+    return new User(user.id, user.username, true)
+  } catch {
     return new User(null, null, false)
-  } else if (response.ok !== true) {
-    throw new Error("Failed to fetch user")
   }
-
-  const user = await response.json()
-  return new User(user.id, user.username, true)
 })
